2df4508104de34a7d1ffd698d0e9175098b107c4
Andres Negrette <andresdngtt@gmail.com>
2022-11-01 21:15:55 -0300

    creacion del proyecto y configuracion inicial

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { ListaService } from '../services/lista.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,64 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(    
+    public alertController: AlertController,
+    public toastController: ToastController,
+    private listaService: ListaService
+
+  ) {}
+
+  async AgregarLista(){
+    let alerta = await this.alertController.create(
+      {
+        header:"Agregar lista",
+        inputs:[
+          {
+            type: "text",
+            name: "titulo",
+            placeholder: "Ingrese el nombre de la lista"
+          }
+        ],
+        buttons:[
+          {
+            text: "Cancelar",
+            role: "cancel"
+          },
+          {
+            text: "Crear",
+            handler: (data: any) => {
+              let isValid: boolean = this.validInput(data);
+              if(isValid){
+                let titulo = data.titulo;
+                let wasCreated = this.listaService.crearLista(titulo);
+                if(wasCreated){
+                  this.presentToast("Lista creada correctamente");
+                }
+              }
+            }
+          }
+        ]
+      });
+    await alerta.present();
+    console.log("Hice click!!");
+  }
+
+  validInput(input: any): boolean{
+    if(input && input.titulo){
+      return true;
+    }
+    this.presentToast("Debe ingresar un valor");
+    return false;
+  }
+
+  async presentToast(mensaje: string): Promise<void> {
+    let toast = await this.toastController.create(
+      {
+        message: mensaje,
+        duration: 2000
+      }
+    );
+    toast.present();
+  }
 
 }
